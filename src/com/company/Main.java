3651d8e62686b55926e7cf3fe5c75907653c3b98
dafiyoung <aiwei1988@gmail.com>
2017-03-20 12:25:35 -0600

    test from MBA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello world!");
         System.out.println("Test of Github!");
+        System.out.println("Test from macbook air");
+
     }
 }
